d94168eb92078bcf332ab07d6d468a6b586c6f45
TheCoolerDuck <162633323+TheCoolerDuck@users.noreply.github.com>
2026-05-30 16:35:40 -0400

    small edit

--- Duck/obj/Debug/net8.0/Duck.AssemblyInfo.cs
+++ Duck/obj/Debug/net8.0/Duck.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Duck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+954f9966a4fba93b669a0d2c8063e42562c0f666")]
 [assembly: System.Reflection.AssemblyProductAttribute("Duck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Duck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
